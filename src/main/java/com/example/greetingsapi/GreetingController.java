bc692ee9a9c54723a5c87de1b0c3972c862e4980
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-07-12 01:04:09 +0530

    added delete operation

--- src/main/java/com/example/greetingsapi/GreetingController.java
+++ src/main/java/com/example/greetingsapi/GreetingController.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,11 @@ public class GreetingController {
     @PutMapping("/editGreeting/{id}")
     public Greeting editGreeting(@RequestBody Greeting greeting, @PathVariable Integer id){
         return service.editGreeting(greeting, id);
+    }
+    @DeleteMapping("/deleteGreeting/{id}")
+    public String deleteGreeting(@PathVariable Integer id){
+        service.deleteGreeting(id);
+        return "Data Deleted";
     }
 }
 	
